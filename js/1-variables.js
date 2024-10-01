b644c3ffaafe2fa043d6b59babb98e17c6fe52ea
tomas <toomiileon@gmail.com>
2024-10-01 11:25:20 -0300

    creamos variables con let y const

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,1 +1,17 @@
+//mensaje que muestro en consola
 console.log('hola mundo');
+
+//declarar o definir una variable (var - let - const)
+let num1;
+
+//declarar e inicializar una variable
+let num2 = 20;
+const pi = 3.14
+
+//asignar el valor a una variable
+num1 = 10;
+
+//mostrar por consola el valor de la variable
+console.log('variable num1 = '+ num1)
+console.log('variable num2 = '+ num2)
+console.log('variable pi = '+ pi)
